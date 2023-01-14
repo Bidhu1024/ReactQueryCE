c7b7b3e3aa23a80f930bc44dcdc37d93d6503f42
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-14 13:00:13 +0530

    Video 4 completed

--- rqce/src/components/RQSuperHeroes.jsx
+++ rqce/src/components/RQSuperHeroes.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import {useQuery} from 'react-query';
-import axios from 'axios';
-
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const RQSuperHeroes = () => {
- const {isLoading,data } =  useQuery('super-heroes',()=>{
-   return axios.get('http://localhost:4000/superheroes')
-  })
-
-  if(isLoading){
-    return <h2>Loading....</h2>
+  const { isLoading, data, error, isError } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+  if (isLoading) {
+    return <h2>Loading....</h2>;
   }
   return (
-    <div><h1> RQSuperHeroes </h1>  
-    {data?.data.map(hero =>{
-      return <div key={hero.name}>{hero.name}</div>
-    })}
-    
+    <div>
+      <h1> RQSuperHeroes </h1>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero?.name}</div>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default RQSuperHeroes
+export default RQSuperHeroes;
--- rqce/src/components/SuperHeroes.jsx
+++ rqce/src/components/SuperHeroes.jsx
@@ -4,17 +4,24 @@ const SuperHeroes = () => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [data,setData] = useState([])
-
+    const [error,setError] = useState('');
 useEffect(()=>{
 axios.get('http://localhost:4000/superheroes').then((res)=>{
     console.log(res)
     setData(res.data)
     setIsLoading(false)
+}).catch(error=>{
+  setError(error.message)
+  setIsLoading(false);
 })
 },[])
 
 if(isLoading){
     return <h2>Loading</h2>
+}
+
+if(error){
+  return <h2>{error}</h2>
 }
 
   return (

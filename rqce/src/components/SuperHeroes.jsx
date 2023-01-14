c3258ec11ce4820398e72973a888ff41584ac85a
Bidhu1024 <bidhu1024av@gmail.com>
2023-01-14 12:34:50 +0530

    Video 2 Completed

--- rqce/src/components/SuperHeroes.jsx
+++ rqce/src/components/SuperHeroes.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 const SuperHeroes = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [data,setData] = useState([])
+
+useEffect(()=>{
+axios.get('http://localhost:4000/superheroes').then((res)=>{
+    console.log(res)
+    setData(res.data)
+    setIsLoading(false)
+})
+},[])
+
+if(isLoading){
+    return <h2>Loading</h2>
+}
+
   return (
-    <div>SuperHeroes</div>
+    <div>
+    <h2> SuperHeroes</h2>
+    {data.map((hero)=>{
+        return <div key={hero.name}>{hero.name}</div>
+    })}
+    
+    </div>
+
   )
 }
 
